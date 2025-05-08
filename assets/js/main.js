0c0d47b3234145d56ffccb44a814ae4cb09f2f7e
Lexra23 <74659632+Lexra23@users.noreply.github.com>
2025-05-08 11:14:43 +0800

    init

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+import '../css/style.css'
+
+var textWrapper = document.querySelector('.title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.title',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
